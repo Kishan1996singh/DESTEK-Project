271ba3ea62700a23d6e8ce8896f60a6926e2a923
Kishan Kumar Singh <159604872+Kishan1996singh@users.noreply.github.com>
2024-10-10 00:28:02 +0530

    Update userMiddleware.js

--- middleware/userMiddleware.js
+++ middleware/userMiddleware.js
@@ -4,11 +4,11 @@ const JWT_SECRET = 'your_jwt_secret';
 // Middleware for authenticating JWT tokens
 const authenticateToken = (req, res, next) => {
   const token = req.headers['authorization'];
-  if (!token) return res.sendStatus(401); // No token provided
+  if (!token) return res.sendStatus(401);
 
   jwt.verify(token, JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403); // Invalid token
-    req.user = user; // Attach user info to request
+    if (err) return res.sendStatus(403);
+    req.user = user;
     next();
   });
 };
